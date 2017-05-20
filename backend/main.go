b34e5a9d2095be3611a414b1aa09adcb378bea01
Antonio Martinović <martinovic.antonio@gmail.com>
2017-05-20 13:13:30 +0200

    points, polygons and users

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,17 @@ import (
 	"os/signal"
 	"fmt"
 	"github.com/pressly/chi"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"net/http/httputil"
+	m "github.com/hackathonovo/do-you-even-code/backend/models"
+	"github.com/jinzhu/gorm"
+	"flag"
+	"github.com/pressly/chi/docgen"
+)
+
+var (
+	db        *gorm.DB
+	genRoutes = flag.Bool("routes", false, "Generate router documentation")
 )
 
 func init() {
@@ -29,8 +39,17 @@ func init() {
 }
 
 func main() {
+	var err error
+	db, err = gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable password=postgres123")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&m.Point{}, &m.User{}, &m.Polygon{})
 
 	router := chi.NewRouter()
+	e := m.NewEnviroment(db)
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		request, err := httputil.DumpRequest(r, true)
@@ -40,6 +59,46 @@ func main() {
 		fmt.Printf("%s \n", request)
 		w.Write([]byte("I am root"))
 	})
+
+	router.Route("/users", func(r chi.Router) {
+		r.Get("/", e.ListUsers)
+		r.Post("/", e.CreateUser)
+	})
+
+	router.Route("/points", func(router chi.Router) {
+		router.Get("/", e.ListPoints)
+		//router.Options("/", Dummy)
+		router.Post("/", e.CreatePoint)
+
+		router.Route("/:pointId", func(r chi.Router) {
+			r.Use(e.PointCtx)
+			r.Get("/", e.GetPoint)
+			r.Put("/", e.UpdatePoint)
+			r.Delete("/", e.DeletePoint)
+		})
+	})
+
+
+	router.Route("/polygons", func(r chi.Router) {
+		r.Post("/", e.CreatePolygon)
+		r.Get("/", e.GetPolygonList)
+		r.Get("/check", e.CheckPointInPoly)
+		r.Route("/:polygonId", func(r2 chi.Router) {
+			r2.Use(e.PolygonCtx)
+			r2.Get("/", e.GetPolygon)
+			r2.Put("/", e.UpdatePolygon)
+			r2.Delete("/", e.DeletePolygon)
+		})
+	})
+
+	if *genRoutes {
+		// fmt.Println(docgen.JSONRoutesDoc(r))
+		fmt.Println(docgen.MarkdownRoutesDoc(router, docgen.MarkdownOpts{
+			ProjectPath: "github.com/TopHatCroat/awesomeProject",
+			Intro:       "Awesome generated docs.",
+		}))
+		return
+	}
 
 	http.ListenAndServe(":3000", router)
 
