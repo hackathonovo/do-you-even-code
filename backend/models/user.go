5f05a82333db0cb9f435c3088abec1bf98706f3f
Antonio Martinović <martinovic.antonio@gmail.com>
2017-05-21 09:17:30 +0200

    updaajsdnoafioiwav

--- backend/models/user.go
+++ backend/models/user.go
@@ -399,7 +399,7 @@ func (e *Env) PushPositionNotification(actionid, userId uint) {
 	var action = Action{}
 	e.DB.Where("id = ?", actionid).First(&action)
 
-	data := map[string]interface{}{"title": "Nova lokacija", "text": "Poziv na akciju spašavanja!", "actionId": action.ID}
+	data := map[string]interface{}{"title": "Nova akcija " + strconv.Itoa(int(actionid)), "text": "Poziv na akciju spašavanja!", "actionId": action.ID}
 	gmsg := gcm.NewMessage(data, []string{users.Fcm}...)
 
 	sender := &gcm.Sender{ApiKey: API_KEY}
