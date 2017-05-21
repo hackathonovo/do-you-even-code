3edca764c7c4c453391e71a35260ac943447ffea
Emil <embalint8@gmail.com>
2017-05-21 03:21:33 +0200

    Update action

--- android/hackathonovo/app/src/main/java/eu/hackathonovo/ui/actions/EditActionsActivity.java
+++ android/hackathonovo/app/src/main/java/eu/hackathonovo/ui/actions/EditActionsActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
@@ -15,6 +18,10 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 import eu.hackathonovo.R;
 import eu.hackathonovo.data.api.models.request.SearchDetailsData;
 import eu.hackathonovo.injection.component.ActivityComponent;
@@ -31,7 +38,18 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
     private RecyclerView mRecyclerView;
     private GridLayoutManager gridLayoutManager;
     private ActionParametersAdapter adapter;
-    List<SearchDetailsData> searchDetailsDatas = new ArrayList<>();
+    private int whichTime;
+    private SearchDetailsData searchDetailsData;
+    private String date;
+    private String time;
+    private boolean sw1,sw2,sw3;
+
+    @BindView(R.id.switch1) Switch aSwitch1;
+    @BindView(R.id.switch2) Switch aSwitch2;
+    @BindView(R.id.switch3) Switch aSwitch3;
+
+    @BindView(R.id.tv_meeting_place) EditText et1;
+    @BindView(R.id.tv_meeting_place2) EditText et2;
 
     public static Intent createIntent(final Context context) {
         return new Intent(context, EditActionsActivity.class);
@@ -40,11 +58,37 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
     @Override
     public void onDateSet(final DatePickerDialog view, final int year, final int monthOfYear, final int dayOfMonth) {
 
+        date = year + "-0" + (monthOfYear + 1) + "-" + dayOfMonth + "T";
+
+        if(whichTime==1){
+            searchDetailsData.setVrijemeNestanka(date + time);
+        }
+        else {
+            searchDetailsData.setVrijemeSastanka(date + time);
+        }
     }
 
     @Override
     public void onTimeSet(final TimePickerDialog view, final int hourOfDay, final int minute, final int second) {
-
+        String hour;
+        String min;
+        String sec ;
+        if (hourOfDay < 10) {
+            hour = String.valueOf(0) + hourOfDay;
+        } else {
+            hour = String.valueOf(hourOfDay);
+        }
+        if (minute < 10) {
+            min = String.valueOf(0) + minute;
+        } else {
+            min = String.valueOf(minute);
+        }
+        if (second < 10) {
+            sec = String.valueOf(0) + second;
+        } else {
+            sec = String.valueOf(second);
+        }
+        time = hour + ":" + min + ":" + sec+ ".0Z";
     }
 
     @Override
@@ -57,8 +101,8 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_action_parameters);
-        SearchDetailsData searchDetailsData;
 
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -69,15 +113,63 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
         presenter.getAction();
     }
 
-    private void addRecyclerItems() {
-        for (int i = 0; i < searchDetailsDatas.size(); i++) {
-            adapter.addItem(new ChildItem(searchDetailsDatas.get(i)));
+    @OnClick(R.id.change_time1_btn)
+    public void change_time1_btnClicked() {
+        setDateTimePicker();
+        whichTime=1;
+    }
+
+    @OnClick(R.id.change_time2_btn)
+    public void change_time2_btnClicked() {
+        setDateTimePicker();
+        whichTime=2;
+    }
+    @OnCheckedChanged(R.id.switch1)
+    public void switch1btnClicked() {
+        if(aSwitch1!=null) {
+            sw1 = aSwitch1.isChecked();
+
+            searchDetailsData.setOzlijeden(String.valueOf(sw1));
         }
+    }
+
+
+    @OnCheckedChanged(R.id.switch2)
+    public void switch2btnClicked() {
+        if(aSwitch2!=null) {
+            sw2 = aSwitch2.isChecked();
+            searchDetailsData.setHitnost(String.valueOf(sw2));
+        }
+    }
+
+    @OnCheckedChanged(R.id.switch3)
+    public void switch3btnClicked() {
+        if(aSwitch3!=null) {
+            sw3 = aSwitch3.isChecked();
+
+            searchDetailsData.setHitnost(String.valueOf(sw3));
+        }
+    }
+
+    @OnClick(R.id.change_time3_btn)
+    public void change_time3_btnClicked() {
+        sendData();
+    }
+
+    private void sendData() {
+        if(!et1.getText().equals("")){
+            searchDetailsData.setLokacija(et1.getText().toString());
+        }
+        if(!et2.getText().equals("")){
+            searchDetailsData.setMjestoSastanka(et2.getText().toString());
+        }
+
+
     }
 
     @Override
     public void renderView(final SearchDetailsData searchDetailsData) {
-
+        this.searchDetailsData=searchDetailsData;
     }
 
     private void setDateTimePicker() {
@@ -90,7 +182,11 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
         );
         datePickerDialog.show(getFragmentManager(), DATE_PICKER_DIALOG_TAG);
         datePickerDialog.setVersion(DatePickerDialog.Version.VERSION_2);
-        datePickerDialog.setTitle("Odaberite datum nesreće");
+        if(whichTime==1)
+            datePickerDialog.setTitle("Odaberite datum nesreće");
+        else {
+            datePickerDialog.setTitle("Odaberite datum sastanka");
+        }
         //datePickerDialog.set(calendar);
         //datePickerDialog.setOnCancelListener(this);
 
@@ -102,7 +198,10 @@ public class EditActionsActivity extends BaseActivity implements EditActionView,
         );
         tpd.show(getFragmentManager(), DATE_PICKER_DIALOG_TAG);
         tpd.setVersion(TimePickerDialog.Version.VERSION_2);
-        datePickerDialog.setTitle("Odaberite vrijeme nesreće");
+        if(whichTime==1)
+            datePickerDialog.setTitle("Odaberite vrijeme nesreće");
+        else
+            datePickerDialog.setTitle("Odaberite vrijeme sastanka");
         //datePickerDialog.setMinDate(calendar);
     }
 }
